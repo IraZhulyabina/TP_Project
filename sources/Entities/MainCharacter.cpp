b0f4ae0f3b951fba232071227d614938339d739d
= <starokozhko.ig>
2022-04-09 02:29:44 +0300

    MainCharacter simple implementation to test drawing

--- sources/Entities/MainCharacter.cpp
+++ sources/Entities/MainCharacter.cpp
@@ -2,4 +2,14 @@
 // Created by profidoc on 20.03.2022.
 //
 
-#include "../../include/Entities/MainCharacter.h"
+#include "include/Entities/MainCharacter.h"
+
+void MainCharacter::DrawingUpdate() {
+  Drawable::SetSpritePosition(Entity::position_);
+}
+
+MainCharacter::MainCharacter() {
+  Drawable::tile_set_name_ = TexturePackResources::Player;
+  Entity::position_ = {10, 10};
+  // TODO: make a setter, when Physics implemented
+}
