1eabdd8a0e4e8a6a135dd57f9b2291b21dc0a5fd
= <starokozhko.ig>
2022-04-09 02:25:26 +0300

    Really strange error because of this delete constructor...

--- include/drawing/TileSet.h
+++ include/drawing/TileSet.h
@@ -7,12 +7,13 @@
 
 class TileSet {  // интерфейс к текстурпаку, вырезающий нужный кусок
  public:
+  TileSet() = default;
   explicit TileSet(const Rect2u& sub_table);
   const Coord2u& GetSubTableCoords() const;
   //  TODO: iterator for Shapes
 
  private:
-  Rect2u sub_table_;
+  Rect2u sub_table_ = {0, 0, 0, 0};
   Coord2u cur_rect_coords_ = {0, 0};  // TODO: make an iterator, not a field
 };
 
