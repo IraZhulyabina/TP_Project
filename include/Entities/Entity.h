3d665c63dfe28c8f87efe0357a0611549520d5c1
= <starokozhko.ig>
2022-04-09 02:34:30 +0300

    add position for Entity.cpp

--- include/Entities/Entity.h
+++ include/Entities/Entity.h
@@ -6,6 +6,10 @@
 #include "include/main.h"
 
 class Entity {
+ public:
+  Entity() = default;
+  explicit Entity(const Coord2f& position);
+
  protected:
-  Coord2f position_;
+  Coord2f position_ = {0, 0};
 };
--- sources/Entities/Entity.cpp
+++ sources/Entities/Entity.cpp
@@ -2,4 +2,8 @@
 // Created by profidoc on 20.03.2022.
 //
 
-#include "../../include/Entities/Entity.h"
+#include "include/Entities/Entity.h"
+
+Entity::Entity(const Coord2f& position) {
+  position_ = position;
+}
