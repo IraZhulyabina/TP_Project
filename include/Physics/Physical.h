960818b203c9b24083b635ac505606080f75e0f2
= <starokozhko.ig>
2022-04-10 02:31:43 +0300

    Physical implemented on simple level

--- include/Physics/Physical.h
+++ include/Physics/Physical.h
@@ -3,7 +3,18 @@
 //
 
 #pragma once
+#include "include/main.h"
 
 class Physical {
+ public:
+  const Rect2f& GetPhysicalShape() const;
+  void SetPhysicalShape(const Rect2f& physical_shape);
+  void SetPosition(const Coord2f& position);
+  void Move(const Coord2f& position);
+  bool Contains(Coord2f& point);
+  bool Intersects(const Physical& other);
+  // TODO: intersection with strait line
 
+ protected:
+  Rect2f physical_shape_;  // TODO: non-rectangle objects?
 };
--- sources/Physics/Physical.cpp
+++ sources/Physics/Physical.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "include/Physics/Physical.h"
+
+const Rect2f& Physical::GetPhysicalShape() const { return physical_shape_; }
+
+void Physical::SetPhysicalShape(const Rect2f& physical_shape) {
+  physical_shape_ = physical_shape;
+}
+
+void Physical::SetPosition(const Coord2f& position) {
+  physical_shape_.left = position.x;
+  physical_shape_.top = position.y;
+}
+
+void Physical::Move(const Coord2f& position) {
+  physical_shape_.left += position.x;
+  physical_shape_.top += position.y;
+}
+
+bool Physical::Contains(Coord2f& point) {
+  return physical_shape_.contains(point);
+}
+
+bool Physical::Intersects(const Physical& other) {
+  return physical_shape_.intersects(other.physical_shape_);
+}
