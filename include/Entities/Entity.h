a52704ec07c9c0f5ac671de4b716accab870ebaa
= <starokozhko.ig>
2022-04-10 02:32:22 +0300

    Entity positional methods implemented

--- include/Entities/Entity.h
+++ include/Entities/Entity.h
@@ -9,6 +9,9 @@ class Entity {
  public:
   Entity() = default;
   explicit Entity(const Coord2f& position);
+  Coord2f& GetPosition();
+  void SetPosition(const Coord2f& position);
+  void Move(const Coord2f& delta);
 
  protected:
   Coord2f position_ = {0, 0};
--- sources/Entities/Entity.cpp
+++ sources/Entities/Entity.cpp
@@ -4,6 +4,10 @@
 
 #include "include/Entities/Entity.h"
 
-Entity::Entity(const Coord2f& position) {
-  position_ = position;
-}
+Entity::Entity(const Coord2f& position) { position_ = position; }
+
+Coord2f& Entity::GetPosition() { return position_; }
+
+void Entity::SetPosition(const Coord2f& position) { position_ = position; }
+
+void Entity::Move(const Coord2f& delta) {}
