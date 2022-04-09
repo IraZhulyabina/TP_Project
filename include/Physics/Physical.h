6a598ec5ea1a2abaf5c4bace1a4115b4f1151a03
= <starokozhko.ig>
2022-04-10 02:05:17 +0300

    pragma once

--- include/Physics/Physical.h
+++ include/Physics/Physical.h
@@ -2,11 +2,8 @@
 // Created by profidoc on 20.03.2022.
 //
 
-#ifndef TP_SOURCES_ENTITIES_PHYSICS_H_
-#define TP_SOURCES_ENTITIES_PHYSICS_H_
+#pragma once
 
 class Physical {
 
 };
-
-#endif //TP_SOURCES_ENTITIES_PHYSICS_H_
