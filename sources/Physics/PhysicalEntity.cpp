2b9c0e11c4f857f7358f12fe45f48219b8005629
= <starokozhko.ig>
2022-04-11 19:14:39 +0300

    Physical -> Physical Entity.

---
+++ sources/Physics/PhysicalEntity.cpp
@@ -0,0 +1,35 @@
+//
+// Created by profidoc on 20.03.2022.
+//
+
+#include "include/Physics/PhysicalEntity.h"
+
+const Rect2f& PhysicalEntity::GetPhysicalShape() const { return physical_shape_; }
+
+void PhysicalEntity::SetPhysicalShape(const Rect2f& physical_shape) {
+  physical_shape_ = physical_shape;
+}
+
+void PhysicalEntity::SetPhysicalPosition(const Coord2f& position) {
+  physical_shape_.left = position.x;
+  physical_shape_.top = position.y;
+}
+
+void PhysicalEntity::MovePhysical(const Coord2f& position) {
+  physical_shape_.left += position.x;
+  physical_shape_.top += position.y;
+}
+
+bool PhysicalEntity::Contains(Coord2f& point) {
+  return physical_shape_.contains(point);
+}
+
+bool PhysicalEntity::Intersects(const PhysicalEntity& other) {
+  return physical_shape_.intersects(other.physical_shape_);
+}
+
+float PhysicalEntity::GetSpeed() const { return speed_; }
+
+const Coord2f& PhysicalEntity::GetVelocityDirection() const {
+  return velocity_direction_;
+}
