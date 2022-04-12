7b86df03113674cbd99b8c5f7b06ef78f22aa160
IraZhulyabina <Zhuliabina.ii@phystech.edu>
2022-04-12 15:34:15 +0300

    Added check for intersection of two segments

--- include/BasicStructs/structs.h
+++ include/BasicStructs/structs.h
@@ -13,6 +13,15 @@ using Rect2f = sf::Rect<float>;
 using Rect2u = sf::Rect<uint32_t>;
 using Rect2i = sf::Rect<int32_t>;
 
+class Segment {
+ public:
+  Segment() {}
+  bool Intersects(Segment& other);
+ private:
+  Coord2f top_left_position_;
+  Coord2f bottom_right_position_;
+};
+
 float Coord2fAbs(const Coord2f& vect);
 float Rad(float angle);
 Coord2f UnitVector(float angle);
