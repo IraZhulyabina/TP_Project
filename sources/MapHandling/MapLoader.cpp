ca5e4f8d2b73ea748cbcb8a2b2138005f7387216
IraZhulyabina <Zhuliabina.ii@phystech.edu>
2022-04-27 00:38:08 +0300

    Altered Drawing Step 7: Wrote MapLoad

--- sources/MapHandling/MapLoader.cpp
+++ sources/MapHandling/MapLoader.cpp
@@ -4,4 +4,20 @@
 #include "include/MapHandling/MapLoader.h"
 
 std::vector<std::vector<ssize_t>> MapLoader::LoadMap(const std::string& path) {
+  std::vector<std::vector<ssize_t>> parsed_map;
+  std::string line;
+  std::ifstream file (path);
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      parsed_map.push_back(std::vector<ssize_t>());
+      size_t position = 0;
+      while ((position = line.find(',')) != std::string::npos) {
+        std::string number = line.substr(0, position);
+        parsed_map.back().push_back(static_cast<ssize_t>(std::stoll(number)));
+        line.erase(0, position + 1);
+      }
+      parsed_map.back().push_back(static_cast<ssize_t>(std::stoll(line)));
+    }
+  }
+  return parsed_map;
 }
