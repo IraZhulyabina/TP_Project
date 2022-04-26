30bf7e1add800de1365d24783ef4bfcf4d35f36b
IraZhulyabina <Zhuliabina.ii@phystech.edu>
2022-04-27 00:02:44 +0300

    Altered Drawing Step 6: Added MapLoader

---
+++ include/MapHandling/MapLoader.h
@@ -0,0 +1,11 @@
+//
+// Created by ubuntu on 4/27/22.
+//
+
+#pragma once
+#include <string>
+
+class MapLoader{
+ public:
+  void LoadMap(const std::string& path);
+};
---
+++ sources/MapHandling/MapLoader.cpp
@@ -0,0 +1,6 @@
+//
+// Created by ubuntu on 4/27/22.
+//
+#include "include/MapHandling/MapLoader.h"
+
+void MapLoader::LoadMap(const std::string& path) {}
