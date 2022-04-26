90f49edb5fb2b32b5290810dfa37a89204957790
= <starokozhko.ig>
2022-04-26 18:25:41 +0300

    starting work in MapHandler

--- include/Handlers/MapHandler.h
+++ include/Handlers/MapHandler.h
@@ -2,11 +2,12 @@
 // Created by profidoc on 20.03.2022.
 //
 
-#ifndef TP_SOURCES_MAPHANDLER_H_
-#define TP_SOURCES_MAPHANDLER_H_
+#pragma once
 
 class MapHandler {
+ public:
+  void Init();  // create tile-handlers, register in drawer
+
+ private:
 
 };
-
-#endif //TP_SOURCES_MAPHANDLER_H_
